74821cffc85f442f9ac3a6c0610355148ceffcc1
Monserrat <monse@vkapp.mx>
2019-07-28 16:12:03 -0500

    subiendo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AsideComponent,
+    CardsComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
